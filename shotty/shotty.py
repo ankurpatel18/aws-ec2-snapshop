96d0da23a7588440a799ef3e34457072d6dad1ea
Ankur <ankuricloud@AnkurMacBookPro.local>
2020-07-08 00:16:03 +0530

    Don't start snapshot if it is in already in progress

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -16,6 +16,10 @@ def filter_instances(project):
 
     return instances
 
+def has_pending_snapshot(volume):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0].state == 'pending'
+
 @click.group()
 def cli():
     """Shotty manages snapshot"""
@@ -84,6 +88,8 @@ def create_snapshots(project):
         i.stop()
         i.wait_until_stopped()
         for v in i.volumes.all():
+            if has_pending_snapshot(v):
+                print("Skipping .... {0}, snapshot is in already in process ".format(v.id))
             print("Creating snapshot of {0}...".format(v.id))
             v.create_snapshot(Description="created by python")
         print("Starting {0}...".format(i.id))
